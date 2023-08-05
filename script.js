c2f1a12b6c883cc9acb29b60d5e4018e2fafa021
Ashfaque D <87939111+ashfaqued@users.noreply.github.com>
2023-08-05 19:59:52 +0530

    postoffice project complete
    update project pushed

---
+++ script.js
@@ -0,0 +1,23 @@
+let ipAddress = document.getElementById("ipAddress");
+let getStartedBtnEle = document.getElementById("getStartedBtn");
+
+
+
+
+getStartedBtnEle.addEventListener("click", function(){
+    window.location.href = "./info";
+})
+
+async function getIpAdress(){
+    let received = await fetch('https://api.ipify.org?format=json')
+
+    .then((res) => res.json())
+    .then((data) => ipAddress.innerText = data.ip)
+    .catch((error) => {
+        ipAddress.innerText = "...-....-...";
+        console.log(error);
+    })
+}
+
+
+getIpAdress()
